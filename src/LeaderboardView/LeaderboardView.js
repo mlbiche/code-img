a39a7579c48f0a60992d02f19e4b42ab3f44adb8
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-15 18:15:58 +1000

    Implement a mockup Leaderboard view component - Update the Leaderboard view component render using userscore component and mockup data - Add some CSS positionning

--- src/LeaderboardView/LeaderboardView.js
+++ src/LeaderboardView/LeaderboardView.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import UserScore from '../UserScore/UserScore';
+
+import './LeaderboardView.css';
 
 function LeaderboardView() {
-    return (
-        <p>LeaderboardView works !</p>
-    );
+  return (
+    <div id="learderboard-container">
+      <h2 id="learderboard-title">Leaderboard</h2>
+      <h3>Best uploaders</h3>
+      <UserScore type="upload-score" username="Krystine" score={20} />
+      <UserScore type="upload-score" username="Irma" score={14} />
+      <UserScore type="upload-score" username="Maureen" score={13} />
+      <UserScore type="upload-score" username="Ursella" score={6} />
+      <UserScore type="upload-score" username="Sherley" score={5} />
+      <h3>Most appreciated</h3>
+      <UserScore type="reaction-score" username="Lexus" score={46} />
+      <UserScore type="reaction-score" username="Ern" score={43} />
+      <UserScore type="reaction-score" username="Blaine" score={34} />
+      <UserScore type="reaction-score" username="Cody" score={30} />
+      <UserScore type="reaction-score" username="Mario" score={22} />
+    </div>
+  );
 }
 
 export default LeaderboardView;
