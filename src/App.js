1816198c7b9da8b0ff7f3a774d2bd0759497a101
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-15 16:28:18 +1000

    Add routing - Add react-router-dom module - Define the 3 first basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <p>Welcome - Code-img</p>
+    <Router>
+      <Route exact path='/' /> {/* TODO : Add Front Page component */}
+      <Route exact path='/discussion/:id' /> {/* TODO : Add discussion component */}
+      <Route exact path='/leaderboard' /> {/* TODO : Add leaderboard component */}
+    </Router>
   );
 }
 
