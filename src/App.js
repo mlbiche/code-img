0bc0bb430e946ad5c26942893cf80765d207370c
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-15 17:06:18 +1000

    Implement LeaderboardView component - Implement LeaderboardView component - Add LeaderboardView component to App routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CommonNavbar from './CommonNavbar/CommonNavbar';
+import LeaderboardView from './LeaderboardView/LeaderboardView';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       <Route exact path='/' /> {/* TODO : Add Front Page component */}
       <Route exact path='/discussion/:id' /> {/* TODO : Add discussion component */}
-      <Route exact path='/leaderboard' /> {/* TODO : Add leaderboard component */}
+      <Route exact path='/leaderboard' component={LeaderboardView} />
     </Router>
   );
 }
---
+++ src/LeaderboardView/LeaderboardView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LeaderboardView() {
+    return (
+        <p>LeaderboardView works !</p>
+    );
+}
+
+export default LeaderboardView;
