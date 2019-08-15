4c788f28f68086222a9392c3c5be7c61f62deb72
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-15 18:20:19 +1000

    Fix indentation

--- src/CommonNavbar/CommonNavbar.js
+++ src/CommonNavbar/CommonNavbar.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './CommonNavbar.css'
 
 function CommonNavbar() {
-    return (
-        <div id="common-navbar-container">
-            <div id="brand-name-container">
-                <h1>Code-img</h1>
-            </div>
-            <div id="authentification-container">
-                <button>Sign in</button>
-                <button>Register</button>
-            </div>
-        </div>
-    );
+  return (
+    <div id="common-navbar-container">
+      <div id="brand-name-container">
+        <h1>Code-img</h1>
+      </div>
+      <div id="authentification-container">
+        <button>Sign in</button>
+        <button>Register</button>
+      </div>
+    </div>
+  );
 }
 
 export default CommonNavbar;
--- src/UserScore/UserScore.js
+++ src/UserScore/UserScore.js
@@ -7,26 +7,26 @@ export const UPLOAD_SCORE = 'upload-score';
 export const REACTION_SCORE = 'reaction-score';
 
 function UserScore({ type, username, score }) {
-    return (
-        <div className="user-score-container">
-            <p>{ username }</p>
-            <p>
-                { score } <span>
-                    { type === UPLOAD_SCORE && 'uploaded images' }
-                    { type === REACTION_SCORE && 'like reactions' }
-                </span>
-            </p>
-        </div>
-    );
+  return (
+    <div className="user-score-container">
+      <p>{username}</p>
+      <p>
+        {score} <span>
+          {type === UPLOAD_SCORE && 'uploaded images'}
+          {type === REACTION_SCORE && 'like reactions'}
+        </span>
+      </p>
+    </div>
+  );
 }
 
 UserScore.propTypes = {
-    type: PropTypes.oneOf([
-        UPLOAD_SCORE,
-        REACTION_SCORE
-    ]).isRequired,
-    username: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired
+  type: PropTypes.oneOf([
+    UPLOAD_SCORE,
+    REACTION_SCORE
+  ]).isRequired,
+  username: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired
 };
 
 export default UserScore;
