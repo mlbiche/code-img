a5f82c40c1afae43ee1b999c640a673cf9840cea
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-15 16:51:21 +1000

    Implement the common navbar component - Define the inner elements of the navbar (title and authentification buttons) - Add some CSS positionning for these elements - Include the common navbar component in the App component as it appears on all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CommonNavbar from './CommonNavbar/CommonNavbar';
 
 function App() {
   return (
     <Router>
+      <CommonNavbar />
+
       <Route exact path='/' /> {/* TODO : Add Front Page component */}
       <Route exact path='/discussion/:id' /> {/* TODO : Add discussion component */}
       <Route exact path='/leaderboard' /> {/* TODO : Add leaderboard component */}
---
+++ src/CommonNavbar/CommonNavbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CommonNavbar.css'
+
+function CommonNavbar() {
+    return (
+        <div id="common-navbar-container">
+            <div id="brand-name-container">
+                <h1>Code-img</h1>
+            </div>
+            <div id="authentification-container">
+                <button>Sign in</button>
+                <button>Register</button>
+            </div>
+        </div>
+    );
+}
+
+export default CommonNavbar;
