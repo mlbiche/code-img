11aa2ab6ba22d80b5f080ae112f98ff3f0bdd6f9
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-19 21:47:22 +1000

    Fix missing key in component list in LeaderboardView component

--- src/LeaderboardView/LeaderboardView.js
+++ src/LeaderboardView/LeaderboardView.js
@@ -5,20 +5,20 @@ import './LeaderboardView.css';
 
 // Mock up upload User scores object constants
 const MOCKUP_UPLOAD_SCORES = [
-  { username: 'Krystine', score: 20 },
-  { username: 'Irma', score: 14 },
-  { username: 'Maureen', score: 13 },
-  { username: 'Ursella', score: 6 },
-  { username: 'Sherley', score: 5 }
+  { id: 0, username: 'Krystine', score: 20 },
+  { id: 1, username: 'Irma', score: 14 },
+  { id: 2, username: 'Maureen', score: 13 },
+  { id: 3, username: 'Ursella', score: 6 },
+  { id: 4, username: 'Sherley', score: 5 }
 ];
 
 // Mock up upload User scores object constants
 const MOCKUP_REACTION_SCORES = [
-  { username: 'Lexus', score: 46 },
-  { username: 'Ern', score: 43 },
-  { username: 'Blaine', score: 34 },
-  { username: 'Cody', score: 30 },
-  { username: 'Mario', score: 22 }
+  { id: 5, username: 'Lexus', score: 46 },
+  { id: 6, username: 'Ern', score: 43 },
+  { id: 7, username: 'Blaine', score: 34 },
+  { id: 8, username: 'Cody', score: 30 },
+  { id: 9, username: 'Mario', score: 22 }
 ];
 
 /**
@@ -29,14 +29,24 @@ function LeaderboardView() {
     <div id="learderboard-container">
       <h2 id="learderboard-title">Leaderboard</h2>
       <h3>Best uploaders</h3>
-      {/* Display all the upload scores in different UserScore component */}
+      {/* Display all the upload scores in different UserScore components */}
       {MOCKUP_UPLOAD_SCORES.map((mockupUploadScore) => (
-        <UserScore type={UPLOAD_SCORE} username={mockupUploadScore.username} score={mockupUploadScore.score} />
+        <UserScore
+          type={UPLOAD_SCORE}
+          username={mockupUploadScore.username}
+          score={mockupUploadScore.score}
+          key={mockupUploadScore.id}
+        />
       ))}
       <h3>Most appreciated</h3>
-      {/* Display all the reaction scores in different UserScore component */}
+      {/* Display all the reaction scores in different UserScore components */}
       {MOCKUP_REACTION_SCORES.map((mockupReactionScore) => (
-        <UserScore type={REACTION_SCORE} username={mockupReactionScore.username} score={mockupReactionScore.score} />
+        <UserScore
+          type={REACTION_SCORE}
+          username={mockupReactionScore.username}
+          score={mockupReactionScore.score}
+          key={mockupReactionScore.id}
+        />
       ))}
     </div>
   );
