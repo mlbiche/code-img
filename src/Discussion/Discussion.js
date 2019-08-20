54881abf09ef0af57bee8fea700e33905917aee4
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-08-20 13:15:36 +1000

    Add a link to the discussion in the Discussion component header

--- src/Discussion/Discussion.js
+++ src/Discussion/Discussion.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Discussion.css';
 
-function Discussion({ username, date, image }) {
+function Discussion({ id, username, date, image }) {
   return (
     <div className="discussion-container">
-      <div className="discussion-header">
-        <h3>{username}</h3>
-        <span className="discussion-date">On {date.toDateString()} at {date.toLocaleTimeString()}</span>
-      </div>
+      <Link to={'/discussion/' + id} className="discussion-header-link">
+        <div className="discussion-header">
+          <h3>{username}</h3>
+          <span className="discussion-date">On {date.toDateString()} at {date.toLocaleTimeString()}</span>
+        </div>
+      </Link>
       <img src={image} alt={image} />
     </div>
   );
