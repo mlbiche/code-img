6d3243e39f728a3a5053dbe7d337873395c9ec06
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-01 08:54:38 +1000

    Add body-parser module to specifiy that the API expects JSON

--- API/index.js
+++ API/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('./routes');
 
 const PORT = 3000;
 
 const app = express();
+
+// Specify that body should be encrypted in JSON
+app.use(bodyParser.json());
 
 // Use the router to provide all endpoints to the Express app
 app.use(router);
