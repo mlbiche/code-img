c5464bd1eaa3a9cf635c15d9b1d442598edd9f34
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-01 12:22:22 +1000

    Implement the JWT generation on successful login

--- API/method/authentication/login.js
+++ API/method/authentication/login.js
@@ -1,21 +1,29 @@
 const { User } = require('../../model');
+const jwt = require('jsonwebtoken');
 
 /**
  * Check the provided credentials for user login
  * @param email The user email
  * @param password The hashed salted password
+ * @return The generated token if logging in succeed, an empty string otherwise
  */
 module.exports = (email, password) => {
   // Search for a user in the database with the corresponding 
-  User.find({ email: email, password: password }, (err, matchedUser) => {
+  User.findOne({ email: email, password: password }, (err, matchedUser) => {
     if (err) {
-      console.log(err);
-      // TODO : Handle connection failure
+      console.log(`Connection failure (email: ${email})...`);
 
-      return console.log('Connection failure...');
+      return '';
     }
 
-    // TODO : Handle connection success
-    console.log('Connection success !');
+    // Generate random secret key for encrypting the JSON Web Token
+    const key = require('crypto').randomBytes(64).toString('hex');
+
+    // Encrypt the JSON Web Token using the user ID
+    const token = jwt.sign({ userId: matchedUser._id }, key);
+
+    console.log(`Connection success (userId: ${matchedUser._id})!`);
+
+    return token;
   });
 };
