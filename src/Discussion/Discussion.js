0739e15640245b0d5712dbea7aca9e937422ff59
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-14 13:45:20 +1000

    Redesign Discussion component using React-Bootstrap

--- src/Discussion/Discussion.js
+++ src/Discussion/Discussion.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Col } from 'react-bootstrap';
 
 import './Discussion.css';
 
 function Discussion({ id, username, date, image }) {
   return (
-    <div className="discussion-container">
-      <Link to={'/discussion/' + id} className="discussion-header-link">
+    <Col className="discussion-col" lg md={6} xs={12}>
+      <Link to={'/discussion/' + id} className="discussion-link">
         <div className="discussion-header">
           <h3>{username}</h3>
           <span className="discussion-date">On {date.toDateString()} at {date.toLocaleTimeString()}</span>
         </div>
+        <img src={image} alt={image} />
       </Link>
-      <img src={image} alt={image} />
-    </div>
+    </Col>
   );
 }
 
