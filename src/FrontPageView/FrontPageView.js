40725c3aba841f4bedb660b6f59150480113700f
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-14 13:45:00 +1000

    Redesign FrontPageView component using React-Bootstrap

--- src/FrontPageView/FrontPageView.js
+++ src/FrontPageView/FrontPageView.js
@@ -2,31 +2,37 @@ import React from 'react';
 import Discussion from '../Discussion/Discussion';
 import { MOCKUP_DISCUSSIONS } from '../DiscussionView/DiscussionView';
 import FrontPageNavBar from '../FrontPageNavBar/FrontPageNavBar';
+import { Container, Col, Row } from 'react-bootstrap';
+
 import './FrontPageView.css';
 
 function FrontPageView() {
   return (
-    <div>
-      <FrontPageNavBar />
-      <div id="front-page-container">
-        <div id="front-page-title">
+    <Container id="front-page-container">
+      <Row>
+        <Col>
+          <FrontPageNavBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col id="discussion-title-col">
           <h2>Discussions</h2>
-        </div>
-        <div id="front-page-discussions-container">
-          {
-            MOCKUP_DISCUSSIONS.map((mockup_discussion) => (
-              <Discussion
-                id={mockup_discussion.id}
-                username={mockup_discussion.responses[0].username}
-                date={mockup_discussion.responses[0].date}
-                image={mockup_discussion.responses[0].img}
-                key={mockup_discussion.id}
-              />
-            ))
-          }
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+      <Row>
+        {
+          MOCKUP_DISCUSSIONS.map((mockup_discussion) => (
+            <Discussion
+              id={mockup_discussion.id}
+              username={mockup_discussion.responses[0].username}
+              date={mockup_discussion.responses[0].date}
+              image={mockup_discussion.responses[0].img}
+              key={mockup_discussion.id}
+            />
+          ))
+        }
+      </Row>
+    </Container>
   );
 }
 
