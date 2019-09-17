684380f14a4e928c889f9484c5749c2fda38a694
Zinh Al-Sweedy <zinh.al-sweedy@student.uts.edu.au>
2019-09-17 17:16:59 +1000

    fix the path for the registration view and add the code to empty the fields after registration

--- src/RegistrationView/RegistrationView.js
+++ src/RegistrationView/RegistrationView.js
@@ -35,33 +35,50 @@ class RegistrationView extends Component {
       password: this.state.password
     }
     // Post request to backend
-    fetch('/api/routes/registration', {
+    fetch('http://localhost:3000/registration', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password,
-      }),
-    }).then(res => res.json())
-      .then(json => {
-        console.log('json', json);
-        if (json.success) {
-          this.setState({
-            message: "successful in registering",
-            email: '',
-            password: ''
-          });
-        } else {
-          this.setState({
-            message: "Error in registering"
-          });
+      body: JSON.stringify(
+        newUser
+      ),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => {
+        // Empty the fileds after registeration
+        this.emailInput.value = '';
+        this.usernameInput.value = '';
+        this.passwordInput.value = '';
+        
+        this.setState({ email: '', password: '' });
+        
+        switch (res.status) {
+          case 201:
+            // Show the success alert
+            this.setState({
+              message: "account create successfully"
+            });
+            break;
+          case 409:
+            // Show the wrong credentials alert
+            this.setState({
+              message: "account already exists"
+            });
+            break;
+          case 500:
+            // Show the internal error alert
+            this.setState({
+              message: "fail to create account"
+            });
+            break;
+          default:
+            break;
         }
       }).catch(error => {
         console.log('signup error: ');
         console.log(error)
-      })
+      });
   };
 
   render() {
@@ -70,16 +87,16 @@ class RegistrationView extends Component {
         <form onSubmit={this.submitRegistration}>
           <label>
             Username:
-                <input type="text" placeholder="Enter your username" name="username" onChange={this.changeUsername} />
+                <input type="text" placeholder="Enter your username" ref={(ref) => this.usernameInput = ref} name="username" onChange={this.changeUsername} />
           </label>
           <label>
             Password:
-                <input type="password" placeholder="Enter your password" name="password" onChange={this.changePassword} />
+                <input type="password" placeholder="Enter your password" ref={(ref) => this.passwordInput = ref}name="password" onChange={this.changePassword} />
           </label>
 
           <label>
             Email:
-                <input type="email" placeholder="Enter your email" name="email" onChange={this.changeEmail} />
+                <input type="email" placeholder="Enter your email" ref={(ref) => this.emailInput = ref}name="email" onChange={this.changeEmail} />
           </label>
           <input type="submit" value="Register" />
         </form>
