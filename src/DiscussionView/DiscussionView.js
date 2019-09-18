058b1cd44f4f03abd975216922e1ef5fe062d3aa
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-18 20:45:33 +1000

    Redesign DiscussionView component using React-Bootstrap

--- src/DiscussionView/DiscussionView.js
+++ src/DiscussionView/DiscussionView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
 import DiscussionResponse from '../DiscussionResponse/DiscussionResponse';
 import UploadImage from '../UploadImage/UploadImage';
 
@@ -10,21 +11,21 @@ export const MOCKUP_DISCUSSIONS = [
   {
     id: 0,
     responses: [
-      { 
+      {
         id: 0,
         username: 'Krystine',
         date: new Date(),
         img: require('../mock-img/books-3733892_640.jpg'),
         reactions: []
       },
-      { 
+      {
         id: 1,
         username: 'Ern',
         date: new Date(),
         img: require('../mock-img/mountain-4387827_640.jpg'),
         reactions: []
       },
-      { 
+      {
         id: 2,
         username: 'Krystine',
         date: new Date(),
@@ -71,7 +72,7 @@ export const MOCKUP_DISCUSSIONS = [
         id: 0,
         username: 'Ursella',
         date: new Date(),
-        img: require('../mock-img/the-feather-of-a-bird-4395771_640.jpg'),
+        img: require('../mock-img/sunset-4405820_640.jpg'),
         reactions: []
       }
     ]
@@ -95,7 +96,7 @@ export const MOCKUP_DISCUSSIONS = [
         id: 0,
         username: 'Ursella',
         date: new Date(),
-        img: require('../mock-img/the-feather-of-a-bird-4395771_640.jpg'),
+        img: require('../mock-img/mountain-4387827_640.jpg'),
         reactions: []
       }
     ]
@@ -111,19 +112,23 @@ function DiscussionView({ match }) {
   const mockupDiscussion = MOCKUP_DISCUSSIONS[match.params.id];
 
   return (
-    <div className="discussion-responses-container">
-      {/* Display all the discussion response in different DiscussionResponse components */}
-      {mockupDiscussion.responses.map((response) => (
-        <DiscussionResponse
-          username={response.username}
-          date={response.date}
-          img={response.img}
-          reactions={response.reactions}
-          key={response.id}
-        />
-      ))}
-      <UploadImage />
-    </div>
+    <Container className="main-container">
+      <Row className="discussion-view-row">
+        {/* Display all the discussion response in different DiscussionResponse components */}
+        {mockupDiscussion.responses.map((response) => (
+          <DiscussionResponse
+            username={response.username}
+            date={response.date}
+            img={response.img}
+            reactions={response.reactions}
+            key={response.id}
+          />
+        ))}
+        <Col lg={7} xs={12} className="discussion-view-upload-img-col">
+          <UploadImage />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
