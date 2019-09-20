08053ba49c6794e933cdc112dc60c0e64d5b2505
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-20 16:25:00 +1000

    Implement database connection and disconnection before and after test

---
+++ API/test/test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Connect to the MongoDB database before testing
+before(async () => {
+  try {
+    await mongoose.connect('mongodb://localhost/test_db', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+    console.log('Connected to MongoDB database!');
+  } catch (err) {
+    console.log('MongoDB database connection has failed: killing the test.');
+    console.log(err);
+    process.exit();
+  }
+});
+
+// Disconnect from the the MongoDB database after testing
+after(async () => {
+  try {
+    await mongoose.disconnect();
+
+    console.log('Disconnected from MongoDB database!');
+  } catch(err) {
+    console.log('MongoDB database disconnection has failed.');
+    console.log(err);
+  }
+});
