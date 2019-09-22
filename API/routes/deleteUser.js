c3587a0aa2a88e10ba42faa070e398a03434c788
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-22 12:22:13 +1000

    Update Mongoose remove to deleteOne

--- API/routes/deleteUser.js
+++ API/routes/deleteUser.js
@@ -1,7 +1,7 @@
 const { User } = require('../model/schema/user');
 
 module.exports = (req, res) => {
-  User.remove({ _id: req.params.userId })
+  User.deleteOne({ _id: req.params.userId })
     .exec()
     .then(result => {// successful delete the user with the specified id
       res.status(202);
