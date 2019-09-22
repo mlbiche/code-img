7aaba9b1d031f13c153b9cc1e9987c027c2eb8d3
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-22 12:12:43 +1000

    Move the link to the discussion around the Discussion component

--- src/Discussion/Discussion.js
+++ src/Discussion/Discussion.js
@@ -5,15 +5,15 @@ import './Discussion.css';
 
 function Discussion({ id, username, date, image }) {
   return (
-    <div className="discussion-container">
-      <Link to={'/discussion/' + id} className="discussion-header-link">
+    <Link to={'/discussion/' + id} className="discussion-link">
+      <div className="discussion-container">
         <div className="discussion-header">
           <h3>{username}</h3>
           <span className="discussion-date">On {date.toDateString()} at {date.toLocaleTimeString()}</span>
         </div>
-      </Link>
-      <img src={image} alt={image} />
-    </div>
+        <img src={image} alt={image} />
+      </div>
+    </Link>
   );
 }
 
