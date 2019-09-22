a90f60a8c037178ab501a82b0d5b0c0383698563
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-09-22 12:17:25 +1000

    Update deleteUser endpoint success HTTP code

--- API/routes/deleteUser.js
+++ API/routes/deleteUser.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
   User.remove({ _id: req.params.userId })
     .exec()
     .then(result => {// successful delete the user with the specified id
-      res.status(200);
+      res.status(202);
     })
     .catch(err => {
       console.log(err);
