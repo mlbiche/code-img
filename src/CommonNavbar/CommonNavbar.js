aada1191292c6684ca76bb2ea7c6d467a0c1dd48
Zinh Al-Sweedy <zinh.al-sweedy@student.uts.edu.au>
2019-09-23 16:51:56 +1000

    implement the  bootstrap for navabar component

--- src/CommonNavbar/CommonNavbar.js
+++ src/CommonNavbar/CommonNavbar.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import './CommonNavbar.css'
+// import './CommonNavbar.css'
+import { Navbar, Nav } from 'react-bootstrap';
 
 /**
  * CommonNavbar component
  */
 function CommonNavbar() {
   return (
-    <div id="common-navbar-container">
-      <Link to='/' id="brand-name-link">
-        <div id="brand-name-container">
-          <h1>Code-img</h1>
-        </div>
-      </Link>
-      <div id="authentification-container">
-        <a href="/login"><button>Log in</button></a>
-        <a href="/registration"><button>Register</button></a>
-      </div>
-    </div>
-  );
-}
-
+    // <div id="common-Navbar-container">
+    //   <Link to='/' id="brand-name-link">
+    //     <div id="brand-name-container">
+    //       <h1>Code-img</h1>
+    //     </div>
+    //   </Link>
+    //   <div id="authentification-container">
+    //     <a href="/login"><button>Log in</button></a>
+    //     <a href="/registration"><button>Register</button></a>
+    //   </div>
+    // </div>
+    <Navbar className="bg-dark justify-content-between" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Code-img</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-Navbar-nav" />
+      <Navbar.Collapse id="responsive-Navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/login">Sign in</Nav.Link>
+          <Nav.Link href="/registration">Register</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+      );
+    }
+    
 export default CommonNavbar;
