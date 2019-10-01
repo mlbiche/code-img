cad4ef5329626ca41508f92882ac6dd5a6b8ee79
Zinh Al-Sweedy <zinh.al-sweedy@student.uts.edu.au>
2019-10-01 14:40:53 +1000

    cleanup code after adding the bootstarp

--- src/RegistrationView/RegistrationView.js
+++ src/RegistrationView/RegistrationView.js
@@ -85,42 +85,20 @@ class RegistrationView extends Component {
   render() {
     return (
       <div>
-        {/* <p>{this.state.message}</p> */}
         <label> {this.state.message}</label>
         <Form onSubmit={this.submitRegistration}>
-          {/* <div class="Form-group"> */}
-          {/* <label>Username</label>
-            <input placeholder="Enter your username"
-              ref={(ref) => this.usernameInput = ref}
-              name="username" onChange={this.changeUsername}
-              type="email" class="Form-control" aria-describedby="emailHelp" />
-            <small id="emailHelp" class="Form-text text-muted">We'll never share your email with anyone else.</small>
-          </div> */}
           <Form.Group>
             <Form.Label>Username</Form.Label>
             <Form.Control type="text" placeholder="Enter your username"
               ref={(ref) => this.usernameInput = ref}
               onChange={this.changeUsername} className="Form-control" />
           </Form.Group>
-          {/* <div class="Form-group">
-            <label>Email address</label>
-            <input type="email" placeholder="Enter your email" ref={(ref) => this.emailInput = ref}
-              name="email" onChange={this.changeEmail}
-              class="Form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email address" />
-            <small id="emailHelp" class="Form-text text-muted">We'll never share your email with anyone else.</small>
-          </div> */}
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter your email"
               ref={(ref) => this.emailInput = ref}
               onChange={this.changeEmail} className="Form-control" />
           </Form.Group>
-          {/* <div class="Form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" placeholder="Enter your password"
-              ref={(ref) => this.passwordInput = ref} name="password" onChange={this.changePassword}
-              class="Form-control" id="exampleInputPassword1" />
-          </div> */}
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Enter your password"
