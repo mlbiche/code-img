02e6554f9e5dd59c39745c8c7f7e1fb3f43667c4
Zinh Al-Sweedy <zinh.al-sweedy@student.uts.edu.au>
2019-10-01 14:42:30 +1000

    cleanup the code after adding the bootstrap style

--- src/FrontPageView/FrontPageView.js
+++ src/FrontPageView/FrontPageView.js
@@ -3,63 +3,36 @@ import Discussion from '../Discussion/Discussion';
 import { MOCKUP_DISCUSSIONS } from '../DiscussionView/DiscussionView';
 import FrontPageNavBar from '../FrontPageNavBar/FrontPageNavBar';
 import './FrontPageView.css';
-import { Container, Col, Row, CardColumns } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 
 function FrontPageView() {
   return (
-    // <div>
-    //   {/* <FrontPageNavBar />
-    //   <div id="front-page-container">
-    //     <div id="front-page-title">
-    //       <h2>Discussions</h2> */}
-    //     {/* </div>
-    //     <div id="front-page-discussions-container">
-    //       {
-    //         MOCKUP_DISCUSSIONS.map((mockup_discussion) => (
-    //           <Discussion
-    //             id={mockup_discussion.id}
-    //             username={mockup_discussion.responses[0].username}
-    //             date={mockup_discussion.responses[0].date}
-    //             image={mockup_discussion.responses[0].img}
-    //             key={mockup_discussion.id}
-    //           />
-    //         ))
-    //       }
-    //     </div> */}
-    // //   </div>
-    // // </div>
-    <Container className="main-container">
-    <Row className="frontpage-row">
-      <Col className="frontpage-title-col">
-        <FrontPageNavBar />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} lg={8} md={4} className="frontpage-title-col">
-        <h2>Discussions</h2>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} lg={8} md={4} className="frontpage-title-col">
-        <CardColumns>
-          {
-            MOCKUP_DISCUSSIONS.map((mockup_discussion) => (
-              <Discussion
-                id={mockup_discussion.id}
-                username={mockup_discussion.responses[0].username}
-                date={mockup_discussion.responses[0].date}
-                image={mockup_discussion.responses[0].img}
-                key={mockup_discussion.id}
-              />
-            ))
-          }
-        </CardColumns>
-      </Col>
-    </Row>
-  </Container>
-);
- }
-//   );
-// }
+    <Container>
+      <Row className="frontpage-row">
+        <Col className="frontpage-title-col">
+          <FrontPageNavBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="frontpage-title-col">
+          <h2>Discussions</h2>
+        </Col>
+      </Row>
+      <Row>
+        {
+          MOCKUP_DISCUSSIONS.map((mockup_discussion) => (
+            <Discussion
+              id={mockup_discussion.id}
+              username={mockup_discussion.responses[0].username}
+              date={mockup_discussion.responses[0].date}
+              image={mockup_discussion.responses[0].img}
+              key={mockup_discussion.id}
+            />
+          ))
+        }
+      </Row>
+    </Container >
+  );
+}
 
 export default FrontPageView;
