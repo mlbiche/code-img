d7030a3acc6ef5dcf36bcbdcbc2e51944269b78c
Zinh Al-Sweedy <zinh.al-sweedy@student.uts.edu.au>
2019-10-01 14:43:20 +1000

    cleanup the code after adding the botstrap style for the frontpagenavbar

--- src/FrontPageNavBar/FrontPageNavBar.js
+++ src/FrontPageNavBar/FrontPageNavBar.js
@@ -1,22 +1,12 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './FrontPageNavBar.css';
+import './FrontPageNavBar.css';
 import { Button, ButtonToolbar} from 'react-bootstrap';
 
 function FrontPageNavBar() {
   return (
-
-    // <div id='front-page-navbar-container'>
-    //   <Link to="/leaderboard">
-    //     <button className="front-page-navbar-button">Leaderboard</button>
-    //   </Link>
-    //   <Link>
-    //     <button className="front-page-navbar-button">Upload New</button>
-    //   </Link>
-    // </div>
     <ButtonToolbar aria-label="Toolbar with button groups">
         <Button href="/leaderboard" variant="secondary">Leaderboard</Button>
-      <Button href="#" variant="secondary">Upload New</Button>
+      <Button href="#" variant="secondary" id="front-page-navbar-button">Upload New</Button>
     </ButtonToolbar >
   );
 }
