c3c2a15c0ae1fb208375217ff50be5617ab24c58
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-10-17 16:57:06 +1100

    Improve CommonNavbar component formatting
    - Add documentation
    - Delete dead code
    - Improve naming

--- src/CommonNavbar/CommonNavbar.js
+++ src/CommonNavbar/CommonNavbar.js
@@ -1,7 +1,9 @@
+/**
+ * CommonNavbar component
+ * 
+ * Display the navbar with the brandname and the authentication links
+ */
 import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// import './CommonNavbar.css'
 import { Navbar, Nav } from 'react-bootstrap';
 
 /**
@@ -9,21 +11,10 @@ import { Navbar, Nav } from 'react-bootstrap';
  */
 function CommonNavbar() {
   return (
-    // <div id="common-Navbar-container">
-    //   <Link to='/' id="brand-name-link">
-    //     <div id="brand-name-container">
-    //       <h1>Code-img</h1>
-    //     </div>
-    //   </Link>
-    //   <div id="authentification-container">
-    //     <a href="/login"><button>Log in</button></a>
-    //     <a href="/registration"><button>Register</button></a>
-    //   </div>
-    // </div>
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="/">Code-img</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-Navbar-nav" />
-      <Navbar.Collapse id="responsive-Navbar-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="/login">Sign in</Nav.Link>
           <Nav.Link href="/registration">Register</Nav.Link>
