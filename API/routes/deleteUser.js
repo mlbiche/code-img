d4f809250c1bbc9be9776bf9d95a26a4d763ddd7
Thibaud Malbert <ThibaudPierre.Malbert@student.uts.edu.au>
2019-10-17 17:22:07 +1100

    Add documentation in deleteUser endpoint

--- API/routes/deleteUser.js
+++ API/routes/deleteUser.js
@@ -1,13 +1,28 @@
+/**
+ * DELETE /:userId endpoint callback
+ * 
+ * Delete a user
+ */
+
 const { User } = require('../model/schema/user');
 
+/**
+ * POST /login endpoint callback
+ * Log in a user
+ * @param req The request. It must contains the userId in the params
+ * @param res The response. 202 on success, 500 if internal error
+ */
 module.exports = (req, res) => {
   User.deleteOne({ _id: req.params.userId })
     .exec()
-    .then(result => {// successful delete the user with the specified id
+    .then(result => {
+      // The user with the specified ID has been successfully deleted
       res.status(202).end();
     })
     .catch(err => {
       console.log(err);
+
+      // Return HTTP erro code 500
       res.status(500).json({
         error: err
       });
