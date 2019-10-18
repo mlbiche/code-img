0e3f5423743b3dd6874f1bf22693d56ce725b652
Zinh Al-Sweedy <zinh.al-sweedy@student.uts.edu.au>
2019-10-18 16:30:35 +1100

    change the code from sync to async

--- API/routes/deleteUser.js
+++ API/routes/deleteUser.js
@@ -1,15 +1,18 @@
 const { User } = require('../model/schema/user');
 
-module.exports = (req, res) => {
-  User.deleteOne({ _id: req.params.userId })
-    .exec()
-    .then(result => {// successful delete the user with the specified id
-      res.status(202).end();
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
+module.exports = async (req, res) => {
+  try {
+    await User.deleteOne({ _id: req.params.userId })
+    // .exec()
+    // .then(result => {// successful delete the user with the specified id
+    await res.status(202).end();
+    // })
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err
     });
+  }
+
 };
